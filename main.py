c711dfb787958a3128a786e405d3a847d69a33a7
iamzilfa <zilcyam@gmail.com>
2022-06-02 11:51:50 +0200

    all

--- main.py
+++ main.py
@@ -3,49 +3,42 @@ import random
 print("Welcome to Rock Paper Scissors game!")
 print("-----------------------------------")
 while True:
-    choices = ["R","P","S"]
+    options = ["R","P","S"]
 
-    computer = random.choice(choices)
+    computer = random.choice(options)
     player = None
 
-    while player not in choices:
+    while player not in options:
         player = input("R, P, or S?: ").upper()
 
 
     if player == computer:
-        print("computer: ",computer)
-        print("player: ",player)
+        print("computer:",(computer), "player:",(player))
         print("It's a Tie!")
 
     elif player == "R":
         if computer == "P":
-            print("computer: ", computer)
-            print("player: ", player)
-            print("Sorry You lose!")
+            print("computer:",(computer), "player:",(player))
+            print('computer wins')
         if computer == "S":
-            print("computer: ", computer)
-            print("player: ", player)
-            print("Bingo You win!")
+            print("computer:",(computer), "player:",(player))
+            print('player wins')
 
     elif player == "S":
         if computer == "R":
-            print("computer: ", computer)
-            print("player: ", player)
-            print("Sorry You lose!")
+            print("computer:",(computer), "player:",(player))
+            print('computer wins')
         if computer == "P":
-            print("computer: ", computer)
-            print("player: ", player)
-            print("Bingo You win!")
+            print("computer:",(computer), "player:",(player))
+            print('player wins')
 
     elif player == "P":
         if computer == "S":
-            print("computer: ", computer)
-            print("player: ", player)
-            print("Sorry You lose!")
+            print("computer:",(computer), "player:",(player))
+            print('computer wins')
         if computer == "R":
-            print("computer: ", computer)
-            print("player: ", player)
-            print("Bingo You win!")
+            print("computer:",(computer), "player:",(player))
+            print('player wins')
         
 
     player_again = input("Wanna play again? (Y/N): ").upper()
